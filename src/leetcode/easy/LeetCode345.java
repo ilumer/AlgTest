36685c8fc03d60f252bd2320e359cba7a59a6007
ilumer <18888106831qxq@gmail.com>
2018-01-04 08:48:38 +0800

    fix LeetCode345

--- src/leetcode/easy/LeetCode345.java
+++ src/leetcode/easy/LeetCode345.java
@@ -2,19 +2,21 @@ package leetcode.easy;
 
 public class LeetCode345 {
   public static String reverseVowels(String s){
-    String Vowels = "aeiouAeIOU";
+    String Vowels = "aeiouAEIOU";
     char[] str = s.toCharArray();
     int left= 0,right=s.length()-1;
     while (left<right){
       while (left<right && Vowels.indexOf(s.charAt(left))==-1)
         left++;
-      while (left<right && Vowels.indexOf(s.charAt(right--))==-1)
+      while (left<right && Vowels.indexOf(s.charAt(right))==-1)
         right--;
       char temp = str[left];
       str[left] = str[right];
       str[right] = temp;
+
       left++;
       right--;
+      //避免出现死循环
     }
     return String.valueOf(str);
   }
