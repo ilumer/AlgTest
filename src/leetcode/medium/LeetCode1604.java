88e46f3d5c94e5da0ea67801dc0e0ef3e83b98e7
ilumer <ilumer.qxq@gmail.com>
2023-02-07 22:51:01 +0800

    feat: add LeetCode1604

--- src/leetcode/medium/LeetCode1604.java
+++ src/leetcode/medium/LeetCode1604.java
@@ -11,26 +11,48 @@ import java.util.stream.Collectors;
 public class LeetCode1604 {
     public List<String> alertNames(String[] keyName, String[] keyTime) {
         int length = keyName.length;
-        Map<String, ArrayDeque<String>> map = new HashMap<>();
+        List<String> result = new ArrayList<>();
+        Map<String, List<String>> nameToTime = new HashMap<>();
         for (int i = 0; i < length; i++) {
-            String key = keyName[i];
+            String name = keyName[i];
             String time = keyTime[i];
-            ArrayDeque<String> timeDeque = map.get(key);
-            if (timeDeque == null) {
-                timeDeque = new ArrayDeque<>();
-                map.put(key, timeDeque);
+            List<String> times = nameToTime.computeIfAbsent(name, k -> new ArrayList<>());
+            times.add(time);
+        }
+        for (Map.Entry<String, List<String>> nameToTimeEntry : nameToTime.entrySet()) {
+            String name = nameToTimeEntry.getKey();
+            List<String> times = nameToTimeEntry.getValue().stream().sorted(new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                        String[] aArray = o1.split(":");
+                        String[] bArray = o2.split(":");
+                        int aHour = Integer.parseInt(aArray[0]);
+                        int aMinute = Integer.parseInt(aArray[1]);
+                        int bHour = Integer.parseInt(bArray[0]);
+                        int bMinute = Integer.parseInt(bArray[1]);
+                        // -1 表示 o1 小于02
+                        if (aHour!=bHour){
+                            return aHour-bHour;
+                        }else {
+                            return aMinute-bMinute;
+                        }
+                }
+            }).collect(Collectors.toList());
+            ArrayDeque<String> timeDeque = new ArrayDeque<>();
+            for (String time : times) {
+                String first = timeDeque.peek();
+                while (first != null && !isInOneHour(first, time)) {
+                    timeDeque.poll();
+                    first = timeDeque.peek();
+                }
+                timeDeque.add(time);
+                if (timeDeque.size() >= 3 && !result.contains(name)) {
+                    result.add(name);
+                }
             }
-            String first = timeDeque.peek();
-            while (first != null && !isInOneHour(first, time)) {
-                timeDeque.poll();
-                first = timeDeque.peek();
-            }
-            timeDeque.add(time);
-
         }
 
-        return map.entrySet()
-                .stream().filter(k -> k.getValue().size() >= 3).map(k -> k.getKey()).sorted().collect(Collectors.toList());
+        return result.stream().sorted().collect(Collectors.toList());
     }
 
     public static boolean isInOneHour(String a, String b) {
@@ -55,6 +77,6 @@ public class LeetCode1604 {
     public static void main(String[] args) {
         String[] keyName = new String[]{"daniel", "daniel", "daniel", "luis", "luis", "luis", "luis"};
         String[] keyTime = new String[]{"10:00", "10:40", "11:00", "09:00", "11:00", "13:00", "15:00"};
-        new LeetCode1604().alertNames(keyName, keyTime);
+        System.out.println(new LeetCode1604().alertNames(keyName, keyTime));
     }
 }
